48359f505b5b6db0ae2335c7aeced0f089b24903
Rahul Menon <teveebeats@gmail.com>
2023-04-05 14:18:17 -0400

    Added update value function that updates the value on the screen added a change unit function

--- GUI.py
+++ GUI.py
@@ -1,4 +1,6 @@
 import asyncio
+import sys
+
 import PySimpleGUI as sg
 
 # bluetooth.py>
@@ -10,12 +12,22 @@ max_temp = [sg.Text(text="Max Temp\n\n _ _ _ °F", justification="center", borde
                     background_color="white", font=("Helvetica", 15), size=(10, 4), text_color="black")]
 current_temp = [sg.Text(text="Curr Temp\n\n _ _ _ °F", justification="center", border_width=6, key="Curr Temp",
                         background_color="white", font=("Helvetica", 15), size=(10, 4), text_color="black")]
+
 buttons = [sg.Button("Start/Stop", font=("Helvetica", 15), pad=(5, 5)),
            sg.Button("°C/°F", font=("Helvetica", 15), pad=(5, 5)),
            sg.Button("Quit", font=("Helvetica", 15), pad=(5, 5))]
 
 total_temp = [max_temp, avg_temp, current_temp]
 
+maxT = -sys.maxsize - 1
+current_temp = None
+
+total_temp = 0
+num_temps = 0
+avgT = 0
+
+in_celsius = False
+poll = False
 layout = [
     [sg.Column([buttons], vertical_alignment="bottom"),
      sg.Column(total_temp, vertical_alignment="bottom")]]
@@ -39,11 +51,16 @@ window = sg.Window(title='Autonomous Vehicle GUI', layout=layout)  # Create the 
 
 while True:
     event, values = window.read()  # Read events and values from the window
-    if event == sg.WIN_CLOSED:  # If the user closes the window
+    if event == sg.WIN_CLOSED or event == "Quit":  # If the user closes the window
         break  # Exit the loop and close the window
-    elif event == "Quit":  # If the user clicks the quit button
-        break  # Exit the loop and close the window
-    elif event == "Start/Stop":
-        break
+    if event == "°C/°F" and current_temp is not None:
+        current_temp = change_unit(current_temp, in_celsius)
+        maxT = change_unit(maxT, in_celsius)
+        avgT = change_unit(avgT, in_celsius)
+        in_celsius = not in_celsius
+        update_value("Curr Temp", current_temp, in_celsius)
+        update_value("Max Temp", maxT, in_celsius)
+        update_value("Avg Temp", avgT, in_celsius)
+
 
 window.close()  # Close the window when the loop exits
